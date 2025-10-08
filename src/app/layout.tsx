5b2a9faed7ae33095f81a8d44b62430df81e449f
Kishor Neupane <heyneupkishor@gmail.com>
2025-10-08 20:30:46 +0000

    the nprogress is not still working, check it. its not showing.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,9 +22,7 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
-        <Suspense>
-            <NextNProgressClient />
-        </Suspense>
+        <NextNProgressClient />
         {children}
         <Toaster />
       </body>
