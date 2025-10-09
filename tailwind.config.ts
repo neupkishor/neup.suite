6226f5fec16ba313ca5d4ed292a227b38b6e4d77
Kishor Neupane <heyneupkishor@gmail.com>
2025-10-09 03:48:33 +0000

    500ms

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -98,8 +98,8 @@ export default {
         },
       },
       animation: {
-        'collapsible-down': 'collapsible-down 0.7s ease-out',
-        'collapsible-up': 'collapsible-up 0.7s ease-out',
+        'collapsible-down': 'collapsible-down 0.5s ease-out',
+        'collapsible-up': 'collapsible-up 0.5s ease-out',
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
       },
